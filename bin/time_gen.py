6ead299aa9c8eea741ed739a28a1b3699d1d1767
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2019-08-04 21:33:40 +0530

    Added file path validator

--- bin/time_gen.py
+++ bin/time_gen.py
@@ -20,13 +20,47 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 @click.option('--out_path','-o',help='Location at where the timelapse will be saved.')
 @click.option('--options',help='Additional options to be specified.')
 def main(in_path,out_path,options):
-    """ Generates a timelapse from the input FITS files (directory) and saves it to the given path."""
-    check_input_path(in_path)
+    """ Generates a timelapse from the input FITS files (directory) and saves it to the given path.
+        ----------
+        parameters
+        ----------
+        in_path  : The path to the directory containing the input FITS files (*.fits or *.fits.fz)
+        out_path : The path at which the output timelapse will be saved. If unspecified writes to .\timelapse
+        options  : Additional options.   
+        ----------
+        returns
+        ----------
+        True if timelapse generated successfully.
+    """
+    fits_files = get_file_list(in_path)
+    #TO DO
 
-def check_input_path(path):
+def get_file_list(path):
+    """ Helper function to validate the given in_path. Checks if the path is valid and if valid checks if the 
+        directory contains valid FITS files. (*.fits or *.fits.fz)
+        ----------
+        parameters
+        ----------
+        path : The path from which to retrieve the FITS files.
+        ----------
+        returns
+        ----------
+        List of filenames if path is valid.
+    """
     if not os.path.isdir(path):
         logging.error('Directory invalid')
         sys.exit('Directory invalid')
+    else:
+        fits_list = []
+        # The FITS files are named in numerically ascending order with an optional 
+        for file in os.listdir(path):
+            if file.endswith('fits.fz') or file.endswith('.fits'):
+                fits_list.append(os.path.join(path,file))
+        if len(fits_list) == 0:
+            logging.error('No FITS files found')   
+            sys.exit("No FITS files found in the directory")        
+    return sorted(fits_list)        
+       
         
 # A broad overview of the pipeline
 # 1. Get directory of FITS files. One directory of observations. If download txt file specified, take care of everything.
