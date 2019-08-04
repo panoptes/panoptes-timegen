066995d004ab2fdd787effb2029ae59229d0d269
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2019-08-04 07:18:17 +0530

    Added commandline options

--- bin/time_gen.py
+++ bin/time_gen.py
@@ -6,6 +6,15 @@ import tqdm
 import numpy as np
 import astropy.io.fits as fits
 
+#Step 0: Read command line args
+@click.command()
+@click.option('--in_path','-i',help='Path to the directory containing the FITS files.',required=True)
+@click.option('--out_path','-o',help='Location at where the timelapse will be saved.')
+@click.option('--options',help='Additional options to be specified.')
+def main(in_path,out_path,options):
+    """ Generates a timelapse from the input FITS files (directory) and saves it to the given path."""
+    print(in_path,out_path)
+
 # A broad overview of the pipeline
 # 1. Get directory of FITS files. One directory of observations. If download txt file specified, take care of everything.
 
@@ -22,3 +31,5 @@ import astropy.io.fits as fits
 #Step 1:
 # Check if the given directory is valid. If not valid: exit and show proper usage guidelines
 # If directory is valid, display summary of folder contents and proceed to .
+if __name__ == '__main__':
+    main()
